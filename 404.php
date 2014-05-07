a7ec43a5e94b5f4ee961f334abf2e1a63461f249
growcreative <nathan@sprinkler.net.au>
2014-05-07 10:23:54 +0800

    Blank Theme Install

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+	<section class="error-page">
+		<h1 class="none">Page not found.</h1>
+		<p> Whoops! It seems the page you are looking for is invisible...</p>
+		<p>We wish you the best of luck finding it!..</br>If you don't succeed you can head back to the <a href="/home">home</a> page.</p>
+	</section>
+	
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,19 @@
+			</div>
+			<!-- /wrap & body elements -->
+			
+
+			<footer>
+				<?php wp_loginout(); ?>
+			</footer>
+		
+		</div>
+		<!-- /container with footer elements -->
+		
+			<noscript>
+			      <div id="noscript-warning">website.com works best with JavaScript enabled*. </br>Please turn JavaScript on in your browser preferences.</div>
+			</noscript>
+		<?php wp_footer(); ?>		
+		<!-- analytics -->
+	
+	</body>
+</html>
